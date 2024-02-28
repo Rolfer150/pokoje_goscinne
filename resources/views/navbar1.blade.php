78bb61bba20dcfd83ba3d5a814a165a6bd1ad6b9
Rafal <rbanka221@gmail.com>
2024-02-28 10:56:13 +0100

    Instalacja Tailwind Css, dodanie widokow home, about i navbar

---
+++ resources/views/navbar1.blade.php
@@ -0,0 +1,18 @@
+<div class="relative">
+    <ul class="flex justify-center space-x-12 pt-4 pb-4 text-white bg-[#1e212b]">
+        <li>
+            <a href="#" class=" p-2 rounded-full"><span>Strona główna</span></a>
+        </li>
+        <li>
+            <a href="#" class=" p-2 rounded-full"><span>O nas</span></a>
+        </li>
+        <li>
+            <a href="#" class=" p-2 rounded-full"><span>Galeria</span></a>
+        </li>
+        <li>
+            <a href="#" class=" p-2 rounded-full"><span>Kontakt</span></a>
+        </li>
+    </ul>
+
+    <img src="{{ asset('img/gora-rozciagniete.jpg') }}" alt="obraz" class="absolute w-full h-screen">
+</div>
